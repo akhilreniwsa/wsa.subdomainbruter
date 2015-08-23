eb6ebc87cbe1e18ffbfb74700b5588d6cfd7f2c0
Akhil Reni <akhilreni.wsa@outlook.com>
2015-08-23 12:42:58 +0530

    added Ajax functionality

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <title>
 WeSecureApp Subdomain Scanner V 0 .1 Beta
 </title>
+        <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
 </head>
 <body>
     <h1> This is an early version of subdomain brute forcer made in PHP</h1> <br/>
@@ -11,15 +12,53 @@ WeSecureApp Subdomain Scanner V 0 .1 Beta
     <li>Multi threading- for fast results</li>
     <li>Input your own subdomain posibilities</li>
     <li>Get a txt output file</li>
-    <form action="scan.php" method="POST">
+    <form id="send" action="scan.php" method="GET">
 <br/>Enter your Target Id in the form of <b>target.com</b> <br/>   <br/>
         <input name="target" placeholder="example.com" value="">
     </form>
+    <div id="message"></div><br/>
+    Valid Domains:<br/><div id="response"></div><br/>
 <?php
 //
 @set_time_limit(0);
-$valid = file_get_contents('valid.txt');
-echo "Here you will see All the valid domains:<br/><br/>" .$valid;
+
 ?>
+      <script>
+            $(function() {
+                $("#send").on("submit", function(e) {
+                    e.preventDefault();
+                    $.ajax({
+                        url: $(this).attr("action"),
+                        type: 'GET',
+                        data: $(this).serialize(),
+                        beforeSend: function() {
+                            $("#message").html("Request is proccessing...");
+                        },
+                        success: function(data) {
+                          
+                        }
+                       
+                    });
+                });
+            });
+          
+          
+          
+          setInterval(function()
+                      {
+  
+        $.ajax({
+            url: "valid.txt",
+            async: false,
+            success: function (data){
+                
+                $("#response").html(data);
+            }
+        });
+    
+
+          
+          }, 1000);
+        </script>
 </body>
 </html>
