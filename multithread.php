ff09486179b9d78d0a6872deadce3facef97488e
Akhil Reni <akhilreni.wsa@outlook.com>
2015-08-23 12:42:08 +0530

    Multithread faster and better version
    Added some properties from https://github.com/jmathai/php-multi-curl

--- multithread.php
+++ multithread.php
@@ -1,51 +1,162 @@
 <?php
- 
-function multiRequest($data, $options = array()) {
- 
-  // array of curl handles
-  $curly = array();
-  // data to be returned
-  $result = array();
- 
- 
-  $mh = curl_multi_init();
- 
- //curl setup
-  foreach ($data as $id => $d) {
- 
-    $curly[$id] = curl_init();
- 
-    $url = (is_array($d) && !empty($d['url'])) ? $d['url'] : $d;
-    curl_setopt($curly[$id], CURLOPT_URL,            $url);
-    curl_setopt($curly[$id], CURLOPT_FOLLOWLOCATION, true);
-    curl_setopt($curly[$id], CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 6.2; WOW64; rv:25.0) Gecko/20100101 Firefox/25.0");
-    curl_setopt($curly[$id], CURLOPT_HEADER,         0);
-    curl_setopt($curly[$id], CURLOPT_RETURNTRANSFER, 1);
- 
-    curl_multi_add_handle($mh, $curly[$id]);
-  }
- 
-  // execute 
-  $running = null;
-  do {
-    curl_multi_exec($mh, $running);
-  } while($running > 0);
- 
- 
-  // get http_status_code of each subdomain
-  foreach($curly as $id => $c) {
-    $result[$id] = curl_getinfo($c,CURLINFO_HTTP_CODE);
-    curl_multi_remove_handle($mh, $c);
+
+class EpiCurl
+{
+  const timeout = 3;
+  static $inst = null;
+  static $singleton = 0;
+  private $mc;
+  private $msgs;
+  private $running;
+  private $execStatus;
+  private $selectStatus;
+  private $sleepIncrement = 1.1;
+  private $requests = array();
+  private $responses = array();
+  private $properties = array();
+  function __construct()
+  {
+    if(self::$singleton == 0)
+    {
+      throw new Exception('This class cannot be instantiated by the new keyword.  You must instantiate it using: $obj = EpiCurl::getInstance();');
     }
- 
-  // close multi curl 
-  curl_multi_close($mh);
-  return $result;
+    $this->mc = curl_multi_init();
+    $this->properties = array(
+      'code'  => CURLINFO_HTTP_CODE,
+      'time'  => CURLINFO_TOTAL_TIME,
+      'length'=> CURLINFO_CONTENT_LENGTH_DOWNLOAD,
+      'type'  => CURLINFO_CONTENT_TYPE,
+      'url'   => CURLINFO_EFFECTIVE_URL
+      );
+  }
+  
+  // simplifies example and allows for additional curl options to be passed in via array
+  public function addURL($url,$options=array()) {
+    $ch = curl_init($url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+      curl_setopt($ch, CURLOPT_CONNECTTIMEOUT ,1); 
+
+		foreach($options as $option=>$value) {
+			curl_setopt($ch, $option, $value);
+		}
+		return $this->addCurl($ch);
+  }
+  public function addCurl($ch)
+  {
+    $key = $this->getKey($ch);
+    $this->requests[$key] = $ch;
+    curl_setopt($ch, CURLOPT_HEADERFUNCTION, array($this, 'headerCallback'));
+    $code = curl_multi_add_handle($this->mc, $ch);
+    
+    // (1)
+    if($code === CURLM_OK || $code === CURLM_CALL_MULTI_PERFORM)
+    {
+      do {
+          $code = $this->execStatus = curl_multi_exec($this->mc, $this->running);
+      } while ($this->execStatus === CURLM_CALL_MULTI_PERFORM);
+      return new EpiCurlManager($key);
+    }
+    else
+    {
+      return $code;
+    }
+  }
+  public function getResult($key = null)
+  {
+    if($key != null)
+    {
+      if(isset($this->responses[$key]['data']))
+      {
+        return $this->responses[$key];
+      }
+      $innerSleepInt = $outerSleepInt = 1;
+      while($this->running && ($this->execStatus == CURLM_OK || $this->execStatus == CURLM_CALL_MULTI_PERFORM))
+      {
+        usleep($outerSleepInt);
+        $outerSleepInt *= $this->sleepIncrement;
+        $ms=curl_multi_select($this->mc);
+        if($ms >= CURLM_CALL_MULTI_PERFORM)
+        {
+          do{
+            $this->execStatus = curl_multi_exec($this->mc, $this->running);
+            usleep($innerSleepInt);
+            $innerSleepInt *= $this->sleepIncrement;
+          }while($this->execStatus==CURLM_CALL_MULTI_PERFORM);
+          $innerSleepInt = 1;
+        }
+        $this->storeResponses();
+        if(isset($this->responses[$key]['data']))
+        {
+          return $this->responses[$key];
+        }
+        $runningCurrent = $this->running;
+      }
+      return null;
+    }
+    return false;
+  }
+  
+  public function cleanupResponses()
+  {
+    $this->responses = array();
+  }
+  private function getKey($ch)
+  {
+    return (string)$ch;
+  }
+  private function headerCallback($ch, $header)
+  {
+    $_header = trim($header);
+    $colonPos= strpos($_header, ':');
+    if($colonPos > 0)
+    {
+      $key = substr($_header, 0, $colonPos);
+      $val = preg_replace('/^\W+/','',substr($_header, $colonPos));
+      $this->responses[$this->getKey($ch)]['headers'][$key] = $val;
+    }
+    return strlen($header);
+  }
+  private function storeResponses()
+  {
+    while($done = curl_multi_info_read($this->mc))
+    {
+      $key = (string)$done['handle'];
+      $this->responses[$key]['data'] = curl_multi_getcontent($done['handle']);
+      foreach($this->properties as $name => $const)
+      {
+        $this->responses[$key][$name] = curl_getinfo($done['handle'], $const);
+      }
+      curl_multi_remove_handle($this->mc, $done['handle']);
+      curl_close($done['handle']);
+    }
+  }
+  static function getInstance()
+  {
+    if(self::$inst == null)
+    {
+      self::$singleton = 1;
+      self::$inst = new EpiCurl();
+    }
+    return self::$inst;
+  }
 }
-
-
- 
-
-
- 
-?>
+class EpiCurlManager
+{
+  private $key;
+  private $epiCurl;
+  function __construct($key)
+  {
+    $this->key = $key;
+    $this->epiCurl = EpiCurl::getInstance();
+  }
+  function __get($name)
+  {
+    $responses = $this->epiCurl->getResult($this->key);
+    return $responses[$name];
+  }
+  function __isset($name)
+  {
+    $val = self::__get($name);
+    return empty($val);
+  }
+}
